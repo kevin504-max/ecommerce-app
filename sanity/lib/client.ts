e86fb257316454273926f3fac3b25ee54a03cde7
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-08-14 14:56:17 -0300

    feat: seed product data

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "skyUIdvSVUFfzLfHjdEgGQuGTqbOeOku90KYuZuWxdv5lGlpkSSx3pEtqkw3AFKEYB8fP9MD1vnWdankqfx4OOS0YJ9UYXOuioAoWjP349KNRJo752EDFC3FOTNGxp6lMefMiUM9GgKEevuzQ4bRGXs94wQAtrjfuMC3oBC1jtZk0JZp22dy",
 })
